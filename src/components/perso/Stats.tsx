423bae9f79ad98e7fb047701ebcef1e414e55e2f
Nicolas <72094122+magicniclus@users.noreply.github.com>
2024-06-11 15:17:37 +0200

    Create Stats.tsx

---
+++ src/components/perso/Stats.tsx
@@ -0,0 +1,46 @@
+import {
+  ArrowRightIcon,
+  ArrowTrendingUpIcon,
+  PhoneArrowUpRightIcon,
+  WrenchIcon,
+} from "@heroicons/react/20/solid";
+
+const Stats = () => {
+  return (
+    <section className="w-full relative flex items-center h-full py-5">
+      <div className="mx-auto w-full h-full flex flex-col md:flex-row max-w-5xl items-center justify-between p-6 lg:px-4 md:py-6 py-16 z-10 h-[170px]">
+        <div className="w-[40%] h-full rounded-2xl bg-slate-100 p-3 px-7 flex flex-col justify-center">
+          <h2 className="font-semibold text-slate-700">
+            Comment{" "}
+            <span className="text-yellow-500">Trouver-Mon-Chantier.fr</span>{" "}
+            vous aide à obtenir plus de client ?
+          </h2>
+          <ArrowRightIcon className="h-6 w-6 text-slate-700" />
+        </div>
+        <div className="w-[16%] h-full rounded-2xl bg-slate-100 p-3 px-7 flex flex-col justify-between text-white bg-bgPrimary">
+          <ArrowTrendingUpIcon className="h-8 w-8" />
+          <div className="flex flex-col">
+            <h3 className="font-semibold">+ de 30 000</h3>
+            <p className="text-xs">recherche par mois</p>
+          </div>
+        </div>
+        <div className="w-[16%] h-full rounded-2xl bg-slate-100 p-3 px-7 flex flex-col justify-between text-white bg-bgPrimary">
+          <WrenchIcon className="h-8 w-8" />
+          <div className="flex flex-col">
+            <h3 className="font-semibold">+ de 70 000</h3>
+            <p className="text-xs">artisans</p>
+          </div>
+        </div>{" "}
+        <div className="w-[16%] h-full rounded-2xl bg-slate-100 p-3 px-7 flex flex-col justify-between text-white bg-bgPrimary">
+          <PhoneArrowUpRightIcon className="h-8 w-8" />
+          <div className="flex flex-col">
+            <h3 className="font-semibold">Jusqu’à 15</h3>
+            <p className="text-xs">demandes par jour</p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Stats;
