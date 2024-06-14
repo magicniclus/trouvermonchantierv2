498b26674c61de832ab5bdfda5f3fc6cb46bda9a
Nicolas <72094122+magicniclus@users.noreply.github.com>
2024-06-14 14:57:13 +0200

    Create page.tsx

---
+++ src/app/pricing/checkout/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import Nav from "@/components/tailwindui/nav/Nav";
+
+const page = () => {
+  return (
+    <>
+      <Nav withMenu={false} />
+    </>
+  );
+};
+
+export default page;
