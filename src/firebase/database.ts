bc3509f96a6fbfc31062d2fc2f1c296c30466a50
Nicolas <72094122+magicniclus@users.noreply.github.com>
2024-06-17 14:19:23 +0200

    Update database.ts

--- src/firebase/database.ts
+++ src/firebase/database.ts
@@ -1,13 +1,13 @@
-import { push, ref, set } from "firebase/database";
+import { get, push, ref, set, update } from "firebase/database";
 import { database } from "./firebase.config";
+
+const date = new Date();
+const dateString = date.toISOString().split("T")[0];
+const path = `prospects/${dateString}`;
+const newRef = push(ref(database, path));
 
 const addProspect = (data: Record<string, any>): Promise<string> => {
   return new Promise((resolve, reject) => {
-    const date = new Date();
-    const dateString = date.toISOString().split("T")[0];
-    const path = `prospects/${dateString}`;
-    const newRef = push(ref(database, path));
-
     set(newRef, data)
       .then(() => {
         resolve(newRef.key!);
@@ -19,4 +19,38 @@ const addProspect = (data: Record<string, any>): Promise<string> => {
   });
 };
 
-export { addProspect };
+const getProspect = (uid: string): Promise<any> => {
+  return new Promise((resolve, reject) => {
+    const prospectRef = ref(database, `prospects/${dateString}/${uid}`);
+
+    get(prospectRef)
+      .then((snapshot) => {
+        if (snapshot.exists()) {
+          resolve(snapshot.val());
+        } else {
+          reject("Prospect not found");
+        }
+      })
+      .catch((error) => {
+        console.error("Error fetching prospect: ", error);
+        reject("Failed to fetch prospect");
+      });
+  });
+};
+
+const updateProspect = (
+  uid: string,
+  data: Record<string, any>
+): Promise<void> => {
+  return new Promise((resolve, reject) => {
+    const prospectRef = ref(database, `prospects/${dateString}/${uid}`);
+    update(prospectRef, data)
+      .then(() => resolve())
+      .catch((error) => {
+        console.error("Error updating prospect: ", error);
+        reject("Failed to update prospect");
+      });
+  });
+};
+
+export { addProspect, getProspect, updateProspect };
