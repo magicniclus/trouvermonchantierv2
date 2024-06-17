4eba1ca256dff8b5056900e5b1d42446b7c7071d
Nicolas <72094122+magicniclus@users.noreply.github.com>
2024-06-17 10:41:59 +0200

    Create firebase.config.ts

---
+++ src/firebase/firebase.config.ts
@@ -0,0 +1,21 @@
+import { initializeApp } from "firebase/app";
+import { getDatabase } from "firebase/database";
+
+// Votre configuration Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyAZLlEQ-UIwZtjVpCWuuIfxp1Hj78ypXKk",
+  authDomain: "trouvermonchantier.firebaseapp.com",
+  databaseURL:
+    "https://trouvermonchantier-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "trouvermonchantier",
+  storageBucket: "trouvermonchantier.appspot.com",
+  messagingSenderId: "637542921551",
+  appId: "1:637542921551:web:8c9b360a3fa5764993c13a",
+};
+
+// Initialisez Firebase
+const app = initializeApp(firebaseConfig);
+
+// Exportez l'instance de la base de données
+const database = getDatabase(app);
+export { database };
