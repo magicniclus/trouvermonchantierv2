39d03073190ab91e6868830eae3d918a9fc8df0a
Nicolas <72094122+magicniclus@users.noreply.github.com>
2024-06-18 14:43:57 +0200

    Update page.tsx

--- src/app/pricing/secteur/page.tsx
+++ src/app/pricing/secteur/page.tsx
@@ -10,12 +10,12 @@ const page = () => {
       <main className="min-h-[calc(100vh-80px)]">
         <div className="max-w-5xl mx-auto px-7 md:px-0">
           <h1 className="mx-auto max-w-4xl text-center text-5xl font-bold tracking-tight text-slate-700 mt-7">
-            Selectionné votre{" "}
-            <span className="text-yellow-500">lieux d&apos;exercice</span>
+            Sélectionnez votre{" "}
+            <span className="text-yellow-500">lieu d&apos;exercice.</span>
           </h1>
           <h2 className="text-center my-7">
-            Pour notre projection, nous ajoutons minimum 10km au tour de la zone
-            defini en fonction de la demande
+            Pour notre projection, nous ajoutons un minimum de 10 km autour de
+            la zone définie en fonction de la demande.
           </h2>
         </div>
         <SearchWithCards />
