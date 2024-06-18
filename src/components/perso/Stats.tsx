8f354379374569f7705cfa5402db81d88f4832be
Nicolas <72094122+magicniclus@users.noreply.github.com>
2024-06-18 14:44:01 +0200

    Update Stats.tsx

--- src/components/perso/Stats.tsx
+++ src/components/perso/Stats.tsx
@@ -17,21 +17,21 @@ const Stats = () => {
           </h2>
           <ArrowRightIcon className="h-6 w-6 text-slate-700" />
         </div>
-        <div className="w-full md:w-[16%] h-full rounded-2xl p-3 px-7 flex flex-col justify-between text-white bg-[#084A59] md:mt-0 mt-5">
+        <div className="w-full md:w-[16%] h-full rounded-2xl p-3 px-7 flex flex-col justify-between text-white bg-slate-700 md:mt-0 mt-5">
           <ArrowTrendingUpIcon className="h-8 w-8" />
           <div className="flex flex-col">
             <h3 className="font-semibold">+ de 7000</h3>
             <p className="text-xs">demande de devis par mois</p>
           </div>
         </div>
-        <div className="w-full md:w-[16%] h-full rounded-2xl p-3 px-7 flex flex-col justify-between text-white bg-[#084A59] md:mt-0 mt-5">
+        <div className="w-full md:w-[16%] h-full rounded-2xl p-3 px-7 flex flex-col justify-between text-white bg-slate-700 md:mt-0 mt-5">
           <WrenchIcon className="h-8 w-8" />
           <div className="flex flex-col">
             <h3 className="font-semibold">+ de 70 000</h3>
             <p className="text-xs">artisans tout corps d&apos;état</p>
           </div>
         </div>{" "}
-        <div className="w-full md:w-[16%] h-full rounded-2xl p-3 px-7 flex flex-col justify-between text-white bg-[#084A59] md:mt-0 mt-5">
+        <div className="w-full md:w-[16%] h-full rounded-2xl p-3 px-7 flex flex-col justify-between text-white bg-slate-700 md:mt-0 mt-5">
           <PhoneArrowUpRightIcon className="h-8 w-8" />
           <div className="flex flex-col">
             <h3 className="font-semibold">Jusqu’à 15</h3>
