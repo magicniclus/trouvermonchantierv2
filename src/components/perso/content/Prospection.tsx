ae8c17a762bc4d6c63598480f799856dd96f704a
Nicolas <72094122+magicniclus@users.noreply.github.com>
2024-06-18 14:19:55 +0200

    Update Prospection.tsx

--- src/components/perso/content/Prospection.tsx
+++ src/components/perso/content/Prospection.tsx
@@ -12,26 +12,40 @@ const Prospection = () => {
             />
           </div>
           <div className="md:w-5/12 w-full text-white mt-10 md:mt-0">
-            <h2 className="font-bold text-2xl">
-              Jusqu’à 100% de prospect en client
-            </h2>
-            <div className="w-full max-w-[200px] bg-yellow-500 rounded-full mt-5 h-3" />
-            <p className="text-white/70 mt-5">
-              Augmentez votre visibilité auprès des clients potentiels en quête
-              de vos services. Gagnez du trafic de qualité en apparaissant sur
-              Google et Facebook, les plateformes les plus influentes du web.
+            <h3 className="text-xl font-semibold mt-5">
+              Maximisez Votre Visibilité en Ligne
+            </h3>
+            <p className="text-white mt-3">
+              Augmentez considérablement votre visibilité auprès des clients
+              potentiels grâce à notre stratégie de présence sur les plateformes
+              les plus influentes du web, telles que Google et Facebook. En
+              apparaissant sur ces canaux et avec une page web dédiée munie
+              d&apos;un nom de domaine personnalisé, vous bénéficiez d&apos;un
+              trafic de qualité essentiel pour attirer des prospects qualifiés.
             </p>
-            <p className="text-white/70 mt-5">
-              Impact de la Recherche en Ligne : En France, 80% des consommateurs
-              effectuent des recherches en ligne avant de sélectionner un
-              prestataire. Avec Google dominant plus de 90% du marché des
-              moteurs de recherche, une absence sur cette plateforme équivaut à
-              céder un avantage considérable à vos concurrents.
+            <h3 className="text-xl font-semibold mt-5">
+              Impact de la Recherche en Ligne
+            </h3>
+            <p className="text-white mt-3">
+              En France, 80% des consommateurs effectuent des recherches en
+              ligne avant de choisir un prestataire. Google domine plus de 90%
+              du marché des moteurs de recherche, ce qui signifie qu&apos;une
+              absence sur cette plateforme vous fait perdre un avantage crucial
+              face à vos concurrents. Une page web dédiée renforce votre
+              crédibilité et votre présence en ligne.
             </p>
-            <p className="text-white/70 mt-5">
-              Cette approche vous assure une présence là où les clients
-              cherchent activement des prestataires, maximisant ainsi vos
-              chances de conversion.
+            <h3 className="text-xl font-semibold mt-5">
+              Présence Stratégique et Conversion Optimale
+            </h3>
+            <p className="text-white mt-3">
+              Notre service vous garantit une présence là où les clients
+              cherchent activement des prestataires. En vous positionnant sur
+              les résultats de recherche Google, les annonces ciblées sur
+              Facebook, et avec une page web professionnelle, nous maximisons
+              vos chances de conversion. Chaque prospect qui vous trouve via ces
+              plateformes et votre site dédié est déjà à la recherche de
+              services comme les vôtres, augmentant ainsi les probabilités
+              qu&apos;ils deviennent vos clients fidèles.
             </p>
           </div>
         </div>
