c176b26f2a943f96972751dab6f2e18e99797153
Nicolas <72094122+magicniclus@users.noreply.github.com>
2025-04-23 19:08:35 +0200

    Update Nav.tsx

--- src/components/tailwindui/nav/Nav.tsx
+++ src/components/tailwindui/nav/Nav.tsx
@@ -103,14 +103,16 @@ const Nav = ({ withMenu }: { withMenu?: boolean }) => {
             </a>
           </PopoverGroup>
         )}
-        <div className="hidden lg:flex lg:flex-1 lg:justify-end">
-          <a
-            href="#pricing"
-            className="block px-6 py-3 text-base font-semibold bg-yellow-500 hover:bg-yellow-400 text-white rounded-xl transform hover:scale-105 transition-all duration-300 shadow-lg hover:shadow-xl"
-          >
-            Commander mon site
-          </a>
-        </div>
+        {withMenu !== false && (
+          <div className="hidden lg:flex lg:flex-1 lg:justify-end">
+            <a
+              href="#pricing"
+              className="block px-6 py-3 text-base font-semibold bg-yellow-500 hover:bg-yellow-400 text-white rounded-xl transform hover:scale-105 transition-all duration-300 shadow-lg hover:shadow-xl"
+            >
+              Commander mon site
+            </a>
+          </div>
+        )}
       </nav>
       <Dialog
         className="lg:hidden"
@@ -121,8 +123,12 @@ const Nav = ({ withMenu }: { withMenu?: boolean }) => {
         <DialogPanel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
           <div className="flex items-center justify-between">
             <a href="/" className="-m-1.5 p-1.5">
-              <span className="sr-only">Terabois</span>
-              <img className="h-8 w-auto" src="/logo.png" alt="terabois" />
+              <span className="sr-only">Trouver mon chantier</span>
+              <img
+                className="h-8 w-auto"
+                src="/favicon.png"
+                alt="Trouver mon chantier"
+              />
             </a>
             <button
               type="button"
@@ -157,14 +163,16 @@ const Nav = ({ withMenu }: { withMenu?: boolean }) => {
                   </a>
                 </div>
               )}
-              <div className="py-6">
-                <a
-                  href="#pricing"
-                  className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-                >
-                  Commander mon site
-                </a>
-              </div>{" "}
+              {withMenu && (
+                <div className="py-6">
+                  <a
+                    href="#pricing"
+                    className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                  >
+                    Commander mon site
+                  </a>
+                </div>
+              )}{" "}
             </div>
           </div>
         </DialogPanel>
