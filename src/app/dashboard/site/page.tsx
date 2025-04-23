0f2d0304c63312d70dbfedf8a1048033aef7d813
Nicolas <72094122+magicniclus@users.noreply.github.com>
2025-04-23 19:08:42 +0200

    Create page.tsx

---
+++ src/app/dashboard/site/page.tsx
@@ -0,0 +1,11 @@
+export default function SitePage() {
+  return (
+    <div>
+      <div className="bg-white rounded-lg shadow p-6">
+        <p className="text-gray-600">
+          Configurez et personnalisez votre site web professionnel.
+        </p>
+      </div>
+    </div>
+  );
+}
