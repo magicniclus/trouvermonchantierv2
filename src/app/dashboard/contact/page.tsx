bed877c603d4825cb338a4feaf7011735d2a5b4d
Nicolas <72094122+magicniclus@users.noreply.github.com>
2025-04-23 21:26:00 +0200

    Update page.tsx

--- src/app/dashboard/contact/page.tsx
+++ src/app/dashboard/contact/page.tsx
@@ -1,10 +1,13 @@
+import ContactForm from "@/components/contact/ContactForm";
+
 export default function ContactPage() {
   return (
-    <div>
-      <div className="bg-white rounded-lg shadow p-6">
-        <p className="text-gray-600">
-          Consultez et gérez les demandes de contact reçues.
-        </p>
+    <div className="h-full flex-1 flex-col space-y-8 p-8 md:flex">
+      <div className="space-y-6">
+        <div className="">
+          <h3 className="text-lg font-medium mb-4">Formulaire de contact</h3>
+          <ContactForm />
+        </div>
       </div>
     </div>
   );
