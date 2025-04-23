c54b8edebb8b86fdcb38bf4b7fde7839979e409e
Nicolas <72094122+magicniclus@users.noreply.github.com>
2025-04-23 14:58:00 +0200

    Create marquee.tsx

---
+++ src/components/magicui/marquee.tsx
@@ -0,0 +1,75 @@
+"use client";
+
+import * as React from "react";
+import { cn } from "@/lib/utils";
+
+interface MarqueeProps
+  extends React.DetailedHTMLProps<
+    React.HTMLAttributes<HTMLDivElement>,
+    HTMLDivElement
+  > {
+  /**
+   * Reverse the animation direction
+   * @default false
+   */
+  reverse?: boolean;
+  /**
+   * Pause the animation on hover
+   * @default false
+   */
+  pauseOnHover?: boolean;
+  /**
+   * The content to animate
+   */
+  children: React.ReactNode;
+  /**
+   * Whether to animate vertically instead of horizontally
+   * @default false
+   */
+  vertical?: boolean;
+  /**
+   * Number of times to repeat the content
+   * @default 4
+   */
+  repeat?: number;
+}
+
+export function Marquee({
+  className,
+  reverse = false,
+  pauseOnHover = false,
+  children,
+  vertical = false,
+  repeat = 4,
+  ...props
+}: MarqueeProps) {
+  return (
+    <div
+      {...props}
+      className={cn(
+        "group flex overflow-hidden p-2 [--duration:40s] [--gap:1rem] [gap:var(--gap)]",
+        {
+          "flex-row": !vertical,
+          "flex-col": vertical,
+        },
+        className,
+      )}
+    >
+      {Array(repeat)
+        .fill(0)
+        .map((_, i) => (
+          <div
+            key={i}
+            className={cn("flex shrink-0 justify-around [gap:var(--gap)]", {
+              "animate-marquee flex-row": !vertical,
+              "animate-marquee-vertical flex-col": vertical,
+              "group-hover:[animation-play-state:paused]": pauseOnHover,
+              "[animation-direction:reverse]": reverse,
+            })}
+          >
+            {children}
+          </div>
+        ))}
+    </div>
+  );
+}
