e96d125253e745cb64b3966acb3415e5953f2a20
Nicolas <72094122+magicniclus@users.noreply.github.com>
2025-04-23 19:08:44 +0200

    Create page.tsx

---
+++ src/app/dashboard/settings/page.tsx
@@ -0,0 +1,11 @@
+export default function ParametresPage() {
+  return (
+    <div>
+      <div className="bg-white rounded-lg shadow p-6">
+        <p className="text-gray-600">
+          Personnalisez les paramètres de votre compte.
+        </p>
+      </div>
+    </div>
+  );
+}
