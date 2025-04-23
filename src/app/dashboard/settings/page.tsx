46af20265defb4c7936448f8a88c664173a0a9aa
Nicolas <72094122+magicniclus@users.noreply.github.com>
2025-04-23 21:26:10 +0200

    Update page.tsx

--- src/app/dashboard/settings/page.tsx
+++ src/app/dashboard/settings/page.tsx
@@ -1,11 +1,7 @@
 export default function ParametresPage() {
   return (
-    <div>
-      <div className="bg-white rounded-lg shadow p-6">
-        <p className="text-gray-600">
-          Personnalisez les paramètres de votre compte.
-        </p>
-      </div>
+    <div className="h-full flex-1 flex-col space-y-8 p-8 md:flex">
+      <div className="flex items-center justify-between space-y-2"></div>
     </div>
   );
 }
