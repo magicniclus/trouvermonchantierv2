eb8ed8eb2ef24ab28ea80aeedbc19feed6421e33
Nicolas <72094122+magicniclus@users.noreply.github.com>
2025-04-25 13:17:58 +0200

    Create page.tsx

---
+++ src/app/v2/page.tsx
@@ -0,0 +1,69 @@
+/* eslint-disable @next/next/no-img-element */
+"use client";
+
+import Stats from "@/components/perso/Stats";
+import Banner from "@/components/perso/banner/Banner";
+import Commentaires from "@/components/perso/content/Commentaires";
+import FAQ from "@/components/perso/content/FAQ";
+import Fonctionnement from "@/components/perso/content/Fonctionnement";
+import Process from "@/components/perso/content/Process";
+import Prospection from "@/components/perso/content/Prospection";
+import Footer from "@/components/perso/footer/Footer";
+import HeroV2 from "@/components/perso/hero/HeroV2";
+import Nav from "@/components/tailwindui/nav/Nav";
+import { useEffect } from "react";
+
+export default function Home() {
+  useEffect(() => {
+    const handleSmoothScroll = (event: MouseEvent) => {
+      const target = event.target as HTMLElement;
+      if (target.matches("a[href^='#']")) {
+        event.preventDefault();
+        const id = target.getAttribute("href")?.slice(1);
+        if (id) {
+          const element = document.getElementById(id);
+          if (element) {
+            element.scrollIntoView({ behavior: "smooth" });
+          }
+        }
+      }
+    };
+
+    document.addEventListener("click", handleSmoothScroll);
+
+    return () => {
+      document.removeEventListener("click", handleSmoothScroll);
+    };
+  }, []);
+  return (
+    <>
+      <header>
+        <Nav />
+      </header>
+      <main className="relative">
+        <HeroV2 />
+        <Stats />
+        <Fonctionnement />
+        <Prospection />
+        <div className="relative w-full overflow-hidden">
+          <img
+            src="/images/Ellipse.png"
+            alt="ellipse"
+            className="absolute -top-[20%] -left-[20%] w-full object-cover h-auto -z-10 opacity-60"
+          />
+          <img
+            src="/images/Ellipse2.png"
+            alt="ellipse"
+            className="absolute top-[50%] -right-[20%] w-full object-cover h-auto -z-10 opacity-60"
+          />
+          {/* <Pricing /> */}
+          <Process />
+          <FAQ />
+          <Commentaires />
+          <Banner />
+        </div>
+      </main>
+      <Footer className="bg-slate-900" />
+    </>
+  );
+}
