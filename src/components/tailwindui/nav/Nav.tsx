47646d7278c8dc06821825d07728246a0b83f641
Nicolas <72094122+magicniclus@users.noreply.github.com>
2025-06-26 13:16:56 +0200

    Update Nav.tsx

--- src/components/tailwindui/nav/Nav.tsx
+++ src/components/tailwindui/nav/Nav.tsx
@@ -62,7 +62,7 @@ const Nav = ({ withMenu }: { withMenu?: boolean }) => {
   return (
     <header className="bg-white">
       <nav
-        className="mx-auto flex max-w-5xl items-center justify-between p-6 lg:px-8"
+        className="mx-auto flex max-w-6xl items-center justify-between p-6 lg:px-8"
         aria-label="Global"
       >
         <a href="/" className="flex lg:flex-1">
