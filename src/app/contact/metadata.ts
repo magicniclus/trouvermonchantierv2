3ab777c59cc00b0ca3ec30a687a2f359cae30038
Nicolas <72094122+magicniclus@users.noreply.github.com>
2025-06-26 14:06:32 +0200

    Create metadata.ts

---
+++ src/app/contact/metadata.ts
@@ -0,0 +1,4 @@
+export const metadata = {
+  title: "Contact - Trouver Mon Chantier",
+  description: "Contactez l'équipe Coudac pour toute question concernant nos services de génération de leads pour artisans.",
+};
