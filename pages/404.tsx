14e405a0c74f1d971e0bde3377c73488d6ab6217
Nicolas <72094122+magicniclus@users.noreply.github.com>
2025-07-18 10:07:20 +0200

    Update 404.tsx

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,15 +1,18 @@
 import Footer from "@/components/perso/footer/Footer";
 import Nav from "@/components/tailwindui/nav/Nav";
+import { MobileMenuProvider } from "@/context/MobileMenuContext";
 
 const Custom404 = () => {
   return (
-    <div>
-      <Nav />
-      <main>
-        <h1>404 - Page Not Found</h1>
-      </main>
-      <Footer />
-    </div>
+    <MobileMenuProvider>
+      <div>
+        <Nav />
+        <main>
+          <h1>404 - Page Not Found</h1>
+        </main>
+        <Footer />
+      </div>
+    </MobileMenuProvider>
   );
 };
 
